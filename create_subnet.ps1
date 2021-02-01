775a5425306c7ccec6fc24267518fc8c8114bd4d
MAULIK RANA <55839387+maulik922@users.noreply.github.com>
2021-02-01 16:18:29 +0530

    Delete create_subnet.ps1

--- create_subnet.ps1
+++
@@ -1,3 +0,0 @@
-﻿$VirtualNetwork = Get-AzVirtualNetwork -ResourceGroupName netrwoking -Name v1 
-Add-AzVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix 10.0.0.0/24 -VirtualNetwork $VirtualNetwork
-$VirtualNetwork | Set-AzVirtualNetwork
