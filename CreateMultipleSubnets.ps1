f62c028b8cfdd00b57c4f0aaf3e3453945779ecc
MAULIK RANA <55839387+maulik922@users.noreply.github.com>
2021-02-01 16:18:22 +0530

    Delete CreateMultipleSubnets.ps1

--- CreateMultipleSubnets.ps1
+++
@@ -1,4 +0,0 @@
-﻿$VirtualNetwork = Get-AzVirtualNetwork -ResourceGroupName netrwoking -Name v1 
-$FrontEnd = Add-AzVirtualNetworkSubnetConfig -Name FrontEnd -AddressPrefix 10.0.3.0/24 -VirtualNetwork $VirtualNetwork
-$BackEnd = Add-AzVirtualNetworkSubnetConfig -Name BackEnd -AddressPrefix 10.0.4.0/24 -VirtualNetwork $VirtualNetwork
-$VirtualNetwork | Set-AzVirtualNetwork
